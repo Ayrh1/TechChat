464155f5df39e6bd3993cc4204d196adaaa1ddd4
ayrh90-fZNzxW <ayrh90@icloud.com>
2023-11-15 19:35:09 -0800

    created seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,38 @@
+//This line imports the Sequelize instance configured for your application. 
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+//importing the models (User, Post, Comment)
+const { User, Post, Comment } = require('../models');
 
+/*
+import the data from JSON files that you will use for seeding. 
+userData, postData, and commentData are arrays of objects
+*/
 const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json'); // Assuming it's a JSON file
 
+/*
+line defines an [asynchronous] function seedDatabase. Asynchronous functions allow to use [await] 
+to pause the function execution until a promise is resolved, 
+which is essential for database operations that are inherently asynchronous.
+*/
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  //[try] block starts here. It allows to execute code and catch any errors that might occur. 
+  try { 
+    await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+    await User.bulkCreate(userData);
 
-  process.exit(0);
+    await Post.bulkCreate(postData);
+
+    await Comment.bulkCreate(commentData);
+
+    console.log('Database seeded!');
+  } catch (error) {
+    console.error('Failed to seed database:', error);
+  } finally {
+    process.exit(0);
+  }
 };
 
 seedDatabase();
